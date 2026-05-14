65a2a3d5de3e3941f8185277ded273d4427eaf22
Thoriq AR <thoriqar@Thoriqs-MacBook-Pro.local>
2026-05-14 13:10:27 +0700

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\TransactionController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -15,5 +16,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/services', ServiceController::class);
 
     Route::apiResource('/customers', CustomerController::class);
+
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+    Route::put('/transactions/{id}/status', [TransactionController::class, 'updateStatus']);
+    Route::post('/transactions/{id}/payment-proof', [TransactionController::class, 'uploadPaymentProof']);
+
+    Route::get('/status-laundry', [TransactionController::class, 'statusLaundry']);
 
 });
