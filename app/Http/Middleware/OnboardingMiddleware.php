dd16f011ba73803c81eb930c9e8db35a2db3bcac
Thoriq AR <thoriqar@Thoriqs-MacBook-Pro.local>
2026-06-14 06:51:00 +0700

    Initial Commit

--- app/Http/Middleware/OnboardingMiddleware.php
+++ app/Http/Middleware/OnboardingMiddleware.php
@@ -5,7 +5,10 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-public function handle(Request $request, Closure $next)
+class OnboardingMiddleware
 {
-    return $next($request);
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
 }
